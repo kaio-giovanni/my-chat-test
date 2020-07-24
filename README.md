695cfcaada69bbe75574828bc8d3c2539db5236e
kaio-giovanni <kaio.giovanni3@gmail.com>
2020-07-24 11:39:49 -0300

    Criação das salas

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# my-chat-test
+# My chat with Socket.io
 Criação de um chat em tempo real com o socket.io.
+
+# Screenshots
+
+<p align="center">
+          <img src="screenshots/screenshot1" />
+</p> 
+<hr />
+<p align="center">
+          <img src="screenshots/screenshot2" />
+</p> 
+<hr />
+<p align="center">
+          <img src="screenshots/screenshot3" />
+</p> 
+<hr />
+
+# Exemplos de uso
+
+* `` io.on("connection", (client) => {...} `` </br>
+  * _Disparado quando o usuário entra na aplicação_
+
+* `` client.emit('message', "this is a test"); `` </br>
+  * _Enviar apenas para o cliente atual_
+
+* `` io.emit('message', "this is a test"); `` </br>
+  * _Enviar para todos os clientes, inclusive o atual_
+
+* `` client.broadcast.emit('message', "this is a test"); `` </br>
+  * _Enviar para todos os clientes, exceto o atual_
+
+* `` socket.broadcast.to('game').emit('message', 'nice game'); `` </br>
+  * _Enviar para todos os clientes (com exceção do atual) para uma sala específica_
+
+* `` io.in('game').emit('message', 'cool game'); `` </br>
+  * _Enviar para todos os clientes em uma sala específica_
+
+* `` client.to('game').emit('message', 'enjoy the game'); `` </br>
+  * _Enviar para o atual, caso ele esteja na sala_
+
+* `` io.of('namespace1').emit('message', 'gg'); `` </br>
+  * _Enviar para todos os clientes em um namespace 'namespace1'_
+
+* `` client.broadcast.to(socketid).emit('message', 'for your eyes only'); `` </br>
+  * _Enviando para um socketid individual_
--- src/server.js
+++ src/server.js
@@ -23,20 +23,45 @@ const server = app.listen(PORT, function () {
 // Socket setup
 const io = socket(server);
 
-io.on('connection', (socket) => {
-  console.log(`Made socket connection ${socket.id}`);
+let listClient = [];
+let listRooms = [];
 
-  let listMsg = [];
+io.on("connection", (client) => {
+  console.log(`User ${client.id} connected`);
 
-  socket.on("sendMessage", (data) => {
-    const messageObject = {
-      "id": socket.id,
-      "author": data.author,
-      "message": data.message
-    }
+  client.on("join", () =>{
+    listClient.push(client.id);
+    client.emit("joined", "You have connected to the server");
+    client.broadcast.emit("joined", client.id + "Has joined to the server");
+  });
 
-    listMsg.push(messageObject);
-    socket.broadcast.emit("receiveMessage", messageObject);
+  client.on("sendMessageAll", (args) => {
+    client.broadcast.emit("receiveMessageAll", {
+      "id": client.id,
+      "author": args.author,
+      "message": args.message
+    });
+  });
+
+  client.on("createRoom", (args) => {
+    client.join(args.room_name);
+    listRooms.push(args.room_name);
+    console.log("user "+ client.id + " has joined in to"+ args.room_name);
+  });
+
+  client.on("sendMessageToRoom", (args) => {
+    client.broadcast.to(args.room_name).emit("receivePrivateMessage", {
+      "id": client.id,
+      "room_name": args.room_name,
+      "author": args.author,
+      "message": args.message
+    });
+  });
+
+  client.on("disconnect", () => {
+    listClient.pop(listClient[client.id]);
+      
+    io.emit("disconnected", client.id + " logout.");
   });
 
 });
